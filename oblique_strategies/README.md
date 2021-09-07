c5a39ac6796377260a4bf5a4ec0b239c594a5ac0
sebhz <seb@zyglute.fr>
2021-09-07 11:05:22 +0200

    Initial commit

---
+++ oblique_strategies/README.md
@@ -0,0 +1,8 @@
+# Oblique strategies
+
+A CLI script to display random Brian Eno and Peter Schmidt [Oblique Strategies](https://en.wikipedia.org/wiki/Oblique_Strategies).
+
+Cards text taken from [Gregory Taylor site](http://www.rtqe.net/ObliqueStrategies/).
+
+Since I was more interested in the spirit rather than the letter of the card, and I do not own a real pack of cards, I made some minor factorization and editorial changes. So I do not guarantee all the texts are fully faithful to the actual cards. But they are very close.
+
---
+++ oblique_strategies/get_strategy.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import argparse
+import random
+import sys
+import oblique_strategies
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-e",
+    "--edition",
+    help="Edition to chose from.",
+    type=int,
+    choices=list(range(1, 6)),
+)
+parser.add_argument(
+    "-a", "--all", help="Display all editions.", action="store_true", default=False
+)
+parser.add_argument(
+    "-s",
+    "--show-edition",
+    help="Show strategy edition(s).",
+    action="store_true",
+    default=False,
+)
+
+_args = parser.parse_args()
+
+if _args.edition:
+    edition = _args.edition - 1
+else:
+    edition = random.randint(0, len(oblique_strategies.STRATEGIES) - 1)
+
+if _args.all:
+    if not _args.edition:
+        all_strats = oblique_strategies.get_all_strats()
+        for strat in sorted(all_strats.keys()):
+            print(strat, all_strats[strat] if _args.show_edition else "")
+    else:
+        for strat in oblique_strategies.STRATEGIES[edition]:
+            print(strat, "[" + str(edition + 1) + "]" if _args.show_edition else "")
+else:
+    print(
+        oblique_strategies.get_random_strat(edition),
+        "[" + str(edition + 1) + "]" if _args.show_edition else "",
+    )
